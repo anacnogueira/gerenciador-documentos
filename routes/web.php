fdcc3f2a73659db3e67da2f75417325e332f9f65
Ana Claudia <anacnogueira@gmail.com>
2025-07-26 12:29:39 -0300

    Rota Home

--- routes/web.php
+++ routes/web.php
@@ -4,5 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 Auth::routes();
 
-Route::get('/', [App\Http\Controllers\Auth\LoginController::class, 'showLoginForm'])->name('home');
+Route::get('/', [App\Http\Controllers\Auth\LoginController::class, 'showLoginForm'])->name('loginForm');
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
